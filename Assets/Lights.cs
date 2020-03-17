7dd5871df57d02c869b269b38362b3ebd37b6f60
ando07 <ando.masahito@gmail.com>
2020-03-17 23:16:20 +0900

    [add]light script

---
+++ Assets/Lights.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+	}
+	
+	public void LightOn()
+	{
+		gameObject.SetActive (true);
+	}
+
+	public void LightOff()
+	{
+		gameObject.SetActive (false);
+	}
+}
